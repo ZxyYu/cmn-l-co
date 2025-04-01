b42c982d97cea2e4a984992b8c5f192bb8ddd95a
zxyYu <14641857+zxyandjyy@user.noreply.gitee.com>
2025-04-01 17:46:59 +0800

    子组件开发

---
+++ app/editor/src/pages/index.tsx
@@ -0,0 +1,12 @@
+// import { Framework } from '../framework'
+
+export const Index = () => {
+  return (
+    <div style={{
+      height: '100vh'
+    }} >
+      {/* <Framework /> */}
+      <h1>Home</h1>
+    </div>
+  );
+};
