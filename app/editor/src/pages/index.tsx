34914b9fe412d6541f7a55095033e3bddfe9cf79
zxyYu <14641857+zxyandjyy@user.noreply.gitee.com>
2025-04-02 13:52:46 +0800

    添加依赖

--- app/editor/src/pages/index.tsx
+++ app/editor/src/pages/index.tsx
@@ -1,12 +1,11 @@
-// import { Framework } from '../framework'
+import { Framework } from '../framework'
 
 export const Index = () => {
   return (
     <div style={{
       height: '100vh'
     }} >
-      {/* <Framework /> */}
-      <h1>Home</h1>
+      <Framework />
     </div>
   );
 };
